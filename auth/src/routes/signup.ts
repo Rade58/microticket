e7a630f58788deed104b9c5b89f6ce896f669211
Rade58 <bajic.rade2@gmail.com>
2021-03-28 18:26:10 +0200

    🦉

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/api/users/signup", (req, res) => {});
+router.post("/api/users/signup", (req, res) => {
+  const { email, password } = req.body;
+});
 
 export { router as signUpRouter };
