d119991284653c1a6f96d3d1af3aa6ee4048dd29
Rade58 <bajic.rade2@gmail.com>
2021-03-28 18:55:42 +0200

    🦉

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 // UVOZIM body IZ express-validator
 // POSTOJI NEKOLIKO NACINA DA SE KORISTI POMENUTI PAKET
 // ALI JA CU KORISTITI OVU body STVAR
@@ -22,12 +22,20 @@ router.post(
     // VIDIS KAKO SE OVO MOZE CHAIN-OVATI
     // PRVO SE PROVERAVA DA LI JE email FIELD NA body-JU
     // PA ONDA SE PROVERAVA DA LI JE REC O EMAIL-U, A ONDA
-    // SAM PODESIO I ERROR MASSEGE, KOJ ICE BITI SENT TO THE USER
+    // SAM PODESIO I ERROR MASSEGE, KOJI BI TREBAO DA SE SEND-UJE TO USER
     // AKO UNESENO NIJE U EMAIL FORMATU
     body("email").isEmail().withMessage("Email must be valid!"),
+    // OVDE JE URADJEN I SANITIZATIO NSTEP, PO KOJEM
+    // SE UKLANJAJU TRIALING SPACES
+    body("password")
+      .trim()
+      .isLength({ max: 20, min: 4 })
+      .withMessage("Pssword must be valid"),
   ],
-  (req, res) => {
-    const { email, password } = req.body;
+  (req: Request, res: Response) => {
+    //
+    // JA SAM GORE PODESIO ERROR MESSAGES, ALI ODAVDE
+    // BI ONE TREBALE DA BUDU SENT
   }
 );
 
