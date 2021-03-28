a6bc140292cf253bfdf5151030d1f5ea0945821a
Rade58 <bajic.rade2@gmail.com>
2021-03-28 15:33:23 +0200

    🦉

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/api/users/current-user", (req, res) => {});
+router.get("/api/users/current-user", (req, res) => {
+  res.send("Hello there Stavros!");
+});
 
 export { router as currentUserRouter };
