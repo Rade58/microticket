88e709c0201ffd33166928601c8c6f394c36d3ce
Rade58 <bajic.rade2@gmail.com>
2021-04-01 18:19:14 +0200

    🦉

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+// UVOZIM MONGOOSE
+import mongoose from "mongoose";
+//
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -23,6 +26,21 @@ app.all("*", async (req, res, next) => {
 });
 
 app.use(errorHandler);
+
+// BUILD-OVACU OVU FUNKCIJU
+const start = async () => {
+  // KORISTIM NAME, OBTAINED FROM CLUSTER IP SERVIZE, ZA POD
+  // U KOJEM RUNN-UJE MONGOV CONTAINER
+  // ALI NE ZABORAVI I PORT ZA DATABASE
+
+  // ZATIM ZADAJ I NAME ZA DATABASE, NA KOJ IZELIS DA SE KONEKTUJES
+  // (STAVI auth ,TO JE NAJVISE APPROPRIATE)
+  await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+};
 
 const PORT = 3000;
 app.listen(PORT, () => {
