714eceb9458dda0a0f0c796e6a548af53a5073ab
Rade58 <bajic.rade2@gmail.com>
2021-04-02 13:48:37 +0200

    🦉

--- auth/src/models/user.model.ts
+++ auth/src/models/user.model.ts
@@ -11,18 +11,28 @@ const userSchema = new Schema({
   },
 });
 
-// UGLAVNOM CU GA ISKORISTITI DA KADA PRISTUPIM
-// REZULTATU QUERY-JA ILI MUTATION-U DA IMAM LISTED FIELD-OVE
-// SA REZULTATA
-interface UserFieldsI extends Document {
+// SLUZICE DA SE TYPE-UJU ARGUMENTI PRI POHRANJIVANJU U DATABASE
+interface UserFieldsI {
   email: string;
   password: string;
 }
 
+// OVAJ INTERFACE, UGLAVNOM CU GA ISKORISTITI DA KADA PRISTUPIM
+// REZULTATU QUERY-JA ILI MUTATION-U DA IMAM LISTED FIELD-OVE
+// SA REZULTATA
+interface UserDocumentI extends Document, UserFieldsI {
+  /* email: string;
+  password: string; */
+  // AKO BUDES IMAO VISE FIELD-OVA
+  // NECU TO MORATI NIGDE DODATNO TYPE-OVATI
+  // JEDINO U GORNJEM UserFieldsI
+  // JER GA ONO EXTEND-UJE
+}
+
 // OMOGUCICE MI DA MOGU DA TYPE-UJEM
 // STATICKE METODE NA MODELU
-interface UserModelI extends Model<UserFieldsI> {
-  buildUser(this: UserModelI, fields: UserFieldsI): Promise<UserFieldsI>;
+interface UserModelI extends Model<UserDocumentI> {
+  buildUser(inputs: UserFieldsI): Promise<UserFieldsI>;
 }
 
 // PROBLEM CE BITI JEDINO STO MORAM TYPE-OVATI this
@@ -30,15 +40,23 @@ interface UserModelI extends Model<UserFieldsI> {
 // ZATO SAM ODMAH NA POCETKU METODE TYPE-OVAO this KEYWORD
 // ISTO TAKO BITNO MI JE DA UPRAVO ZBOG this, OVO NE BUDE
 // ARROW FUNKCIJA
-userSchema.statics.buildUser = async function (fields) {
+userSchema.statics.buildUser = async function (inputs) {
   const User = this as UserModelI;
 
-  const newUser = await User.create(fields);
+  const newUser = await User.create(inputs);
 
   return { email: newUser.email, password: newUser.password };
 };
 
 // SADA NA MODELU MOZES ODMAH UZETII ONU STATICKU METODU
-const User = model<UserFieldsI, UserModelI>("User", userSchema);
+const User = model<UserDocumentI, UserModelI>("User", userSchema);
+
+// PROVERA DA LI CE TYPESCRIPT YELL-OVATI AKO DODAM EXTRA FIELD
+// KOJI NIJE TYPED
+User.buildUser({
+  email: "",
+  password: "",
+  name: "",
+});
 
 export { User };
