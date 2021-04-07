a53dbb87064acd2e444fc6336686ba760c9e71f9
Rade58 <bajic.rade2@gmail.com>
2021-04-07 19:10:34 +0200

    🦉

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -8,6 +8,11 @@ const router = Router();
 
 // DODACU OVDE, POMENUTI MIDDLEWARW
 router.get("/api/users/current-user", currentUser, (req, res) => {
+  // AKO JE KORISNIK AUTHENTICATED IMAS NA req.currentUser USER OBJEKAT
+  // A AKO NEM OBJEKAT ONDA JE req.currentUser, USTVARI null
+
+  // ZATO MI NISTA OVO VISE NE TREBA
+  /*
   if (!req.session || !(req.session as { jwt: string }).jwt) {
     res.send({ currentUser: null });
   }
@@ -22,7 +27,14 @@ router.get("/api/users/current-user", currentUser, (req, res) => {
     console.log(err);
 
     res.send({ currentUser: null });
-  }
+  } */
+
+  // A SALJEM OBJEKAT TO THE USER OVAKO
+  // ALI VODIM RACUNA DA SALJEM null A NE undefined
+  // PROSTO TAKO JE BOLJE
+  res.send({
+    currentUser: !req.currentUser ? null : req.currentUser,
+  });
 });
 
 export { router as currentUserRouter };
