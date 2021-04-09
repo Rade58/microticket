82b9f1adec6457de33999ca3b1e957983c7d04f7
Rade58 <bajic.rade2@gmail.com>
2021-04-09 14:13:37 +0200

    🦉

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -53,8 +53,6 @@ it("returns 400 on invalid email and password, or when email and password are mi
     .expect(400);
 });
 
-//
-
 it("returns 400 if u try to create user with a existing email", async () => {
   await request(app)
     .post("/api/users/signup")
@@ -65,4 +63,21 @@ it("returns 400 if u try to create user with a existing email", async () => {
     .post("/api/users/signup")
     .send({ email: "nickmullen@comedy.com", password: "Sammich284" })
     .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
+  // PRVO PRVIMO SIGNAUP
+  // ODNOSNO TESTIRAMO PRVLJANJE NOVOG USERA, I OCEKUSEMO STATUS CODE 201
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "ian@mail.com",
+      password: "JonTheKingzly6",
+    })
+    .expect(201);
+
+  // ALI KAO STO SI VIDEO CEO GORNJI CALL ETURN-UJE RESPONE
+  // TAKO DA GA MOZEMO INSPECT-OVATI DA VIDIMO DA LI JE NA NJEMU COOKIE
+  // DAKLE OCEKUJEMO DA "Set-Cookie" HEADER RESPONSE OBJEKTA BUDE DEFINED
+  expect(response.get("Set-Cookie")).toBeDefined();
 });
