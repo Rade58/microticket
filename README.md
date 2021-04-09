42f69123c0a91d0563efe92a11ac7f87e2bc8c89
Rade58 <bajic.rade2@gmail.com>
2021-04-09 11:05:59 +0200

    🦉

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # INVALID INPUT TEST; REQUIRING UNIQUE MAIL TEST
 
+DAAKLE TESTIRANJE KORISCENJEMM supertest LIBRARY-JA JE KAKO SI TO MOGAO VIDETI U PREDHODNIM BRNCH-EVIMA VEOMA EASY
+
+# SADA TESTIRAM INVALID INPUTE mail-A I password-A
+
+- `code auth/src/routes/__test__/signup.test.ts`
+
+```ts
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "stvros@test.com",
+      password: "AdamIsCoolBird6",
+    })
+    .expect(201);
+});
+
+// EVO PRAVIM NOVI TEST
+
+it("returns 400 on invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "something",
+      password: "AdamIsCoolBird6",
+    })
+    .expect(400);
+});
+
+```
+
+STARTUJ TEST AKO GA NEMAS STARTOVANOG OD RANIJE
+
+- `cd auth` `yarn test`
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,23 +1,24 @@
-// OVO CE MI OMOGUCITI DA FAKE-UJEM REQUEST TO THE EXPRESS APP
 import request from "supertest";
 import { app } from "../../app";
 
-// OVO PRVO JE DESCRIPTIO ZA TEST, A ZADAJES I CALLBACK
 it("returns 201 on successful signup", async () => {
-  // ZELIMO DA TESTIRAMO SLANJE REQUESTA DO ZADATOG ROUTE-A,
-  // SA ZADATIM HTTP METHOD-OM
-  // TO NAM OMOGUCAVA supertest
-  return (
-    request(app)
-      .post("/api/users/signup")
-      // ZATIM ZELIMO DA SEND-UJEMO SLEDECE
-      .send({
-        email: "stvros@test.com",
-        password: "AdamIsCoolBird6",
-      })
-      // CHAIN-UJEMO DALJE
-      // OVO SU ASSERTIONS (TVRDNJE) ABOUT THE RESPONSE
-      // OCEKUJEMO DAKLE STATUS CODE 201
-      .expect(201)
-  );
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "stvros@test.com",
+      password: "AdamIsCoolBird6",
+    })
+    .expect(201);
+});
+
+// EVO PRAVIM NOVI TEST
+
+it("returns 400 on invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "something",
+      password: "AdamIsCoolBird6",
+    })
+    .expect(400);
 });
