7e1a29e5f0d1a4e90a7cd72bb77b7af8b9b1b967
Rade58 <bajic.rade2@gmail.com>
2021-04-09 21:25:54 +0200

    🦉

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,11 +1,7 @@
 import request from "supertest";
-
 import { app } from "../../app";
 
-// PRVO PRAVIMO TEST, U KOJEM PRAVIM ASSERTION O TOME DA JE USER
-
 it("responds with details about the current user signed in", async () => {
-  // PRAVIMO NOVOG USER-A, I TU EXPECT-UJEMO 201
   await request(app)
     .post("/api/users/signup")
     .send({
@@ -14,13 +10,10 @@ it("responds with details about the current user signed in", async () => {
     })
     .expect(201);
 
-  // OVDE OCEKUJEMO 200 SSTATUS KADA GET-UJEMO CURRENT USER-A
   const response = await request(app)
     .get("/api/users/current-user")
     .send()
     .expect(200);
 
-  // SADA DAKLE DAT O USER-U TREBA DA SE NALAZE NA body-JU
-  // STAMPACU GA CITO DA VIDIM
   console.log(response.body);
 });
