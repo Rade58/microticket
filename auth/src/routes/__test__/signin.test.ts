2098c921e562376cb868888a4053680d39d1d489
Rade58 <bajic.rade2@gmail.com>
2021-04-09 16:33:36 +0200

    🦉

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,2 +1,15 @@
 import request from "supertest";
 import { app } from "../../app";
+
+it("returns 200 on successful signin", async () => {
+  // PRVO MORAMO NAPRAVITI USERA (signup)
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "georgelopez@mail.com", password: "RookieSinger1" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    // MORAMO POSLATI ISTI email I password KAO ABOVE
+    .send({ email: "georgelopez@mail.com", password: "RookieSinger1" });
+});
