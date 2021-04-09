f3db5bcd29f11db509e887670f71d9fb15f4e3d2
Rade58 <bajic.rade2@gmail.com>
2021-04-09 18:11:28 +0200

    🦉

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -34,7 +34,7 @@ it("returns 400 if password is empty string", async () => {
 
 // ....
 
-it("response has a cookie", async () => {
+it("response has a cookie when given valid credentials", async () => {
   await request(app)
     .post("/api/users/signup")
     .send({ email: "stavros@mail.com", password: "RookieSinger1" })
