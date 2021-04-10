317a1e41763907b35cbb452d6768f411ea32a014
Rade58 <bajic.rade2@gmail.com>
2021-04-10 17:36:32 +0200

    🦉

---
+++ client/next.config.js
@@ -0,0 +1,56 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+  /* PHASE_EXPORT,
+  PHASE_PRODUCTION_SERVER, */
+} = require("next/constants");
+
+const dotenvLoad = require("dotenv-load");
+
+const nextEnv = require("next-env");
+const withPlugins = require("next-compose-plugins");
+
+const path = require("path");
+
+// ------------------
+
+dotenvLoad();
+
+// ----------------------------------
+
+const envPlugin = nextEnv();
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) console.log("Development");
+  if (phase === PHASE_PRODUCTION_BUILD) console.log("Production");
+
+  const newConfig = { ...defaultConfig };
+
+  newConfig.webpack = (config, options) => {
+    /* config.module.rules.push({
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      enforce: "post",
+      use: {
+        loader: "ify-loader",
+      },
+    }); */
+
+    return config;
+  };
+
+  // WEBPACK 5 ENABLING
+  newConfig.future = {
+    webpack5: true,
+  };
+
+  // console.log({ newConfig });
+
+  let configuration = withPlugins([envPlugin])(phase, {
+    defaultConfig: newConfig,
+  });
+
+  // console.log({ configuration });
+
+  return configuration;
+};
