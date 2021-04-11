f45a6bcfe6e28691e71109bb033744a8525dca2d
Rade58 <bajic.rade2@gmail.com>
2021-04-11 12:13:10 +0200

    🦉

--- README.md
+++ README.md
@@ -50,3 +50,63 @@ const SignupPage: FunctionComponent = () => {
 
 export default SignupPage;
 ```
+
+# HANDLING email AND password INPUTS
+
+- `code client/pages/auth/signup.tsx`
+
+```tsx
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import { FunctionComponent, useState, useCallback } from "react";
+
+const SignupPage: FunctionComponent = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const sendRequest = useCallback(async () => {
+    // request will be here
+    console.log("Sending Request");
+
+    setEmail("");
+    setPassword("");
+  }, [email, password, setEmail, setPassword]);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        sendRequest();
+      }}
+    >
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label htmlFor="email-signup">Email Address: </label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          id="email-signup"
+          type="email"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password-signup">Password: </label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          id="password-signup"
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default SignupPage;
+```
--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,16 +1,51 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useCallback } from "react";
 
 const SignupPage: FunctionComponent = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const sendRequest = useCallback(async () => {
+    // request will be here
+    console.log("Sending Request");
+
+    setEmail("");
+    setPassword("");
+  }, [email, password, setEmail, setPassword]);
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        sendRequest();
+      }}
+    >
       <h1>Sign Up</h1>
-      <label htmlFor="email-signup">Email Address: </label>
-      <input id="email-signup" type="email" className="form-control" />
-      <label htmlFor="password-signup">Password: </label>
-      <input id="password-signup" type="password" className="form-control" />
-      <button className="btn btn-primary">Sign Up</button>
+      <div className="form-group">
+        <label htmlFor="email-signup">Email Address: </label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          id="email-signup"
+          type="email"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password-signup">Password: </label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          id="password-signup"
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Sign Up
+      </button>
     </form>
   );
 };
