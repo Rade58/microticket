eee0a4d0ca73651824036ec46d3a4fb77c98051b
Rade58 <bajic.rade2@gmail.com>
2021-04-14 18:03:03 +0200

    🦉

--- client/utils/buildApiClient.ts
+++ client/utils/buildApiClient.ts
@@ -6,54 +6,7 @@ export const buildApiClient = () => {
     ? "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local"
     : "";
 
-  // MOGAO SAM SAMO OVO URADITI
   return axios.create({
     baseURL,
   });
-
-  // OVO DOLE JA NISTA NISAM NI MORAO RADITI, I ZATO TO UKLANJAM
-
-  /* return async (
-    path: string,
-    method: "get" | "post",
-    mainHeaders?: HeadersI,
-    body?: any,
-    otherHeaders?: Record<string, string>
-  ) => {
-    if (method === "post" && !body)
-      throw new Error("You didn't provide the body of request");
-
-    const url = baseUrl + (path.startsWith("/") ? "" : "/") + path;
-
-    let headers = {};
-
-    if (otherHeaders) {
-      headers = { ...headers, ...otherHeaders };
-    }
-    if (mainHeaders) {
-      headers = { ...headers, ...mainHeaders };
-    }
-
-    if (isSSR()) {
-      if (method === "get") {
-        return axios[method](url, {
-          headers,
-        });
-      }
-
-      if (method === "post") {
-        return axios[method](url, body, {
-          headers,
-        });
-      }
-    } else {
-      if (method === "get") {
-        return axios[method](url, { headers });
-      }
-
-      if (method === "post") {
-        return axios[method](url, body, { headers });
-      }
-    }
-  }; */
 };
