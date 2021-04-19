dec0aa8aad7358777364432792b43ce8d2e7f535
Rade58 <bajic.rade2@gmail.com>
2021-04-19 13:29:49 +0200

    🦉

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY env variable undefined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI env variable undefined");
+  }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
