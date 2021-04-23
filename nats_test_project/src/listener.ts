a8a79d6c5ea6fa86318e15d3fd35a1a7a3b93bcf
Rade58 <bajic.rade2@gmail.com>
2021-04-23 17:41:09 +0200

    🦉

--- nats_test_project/src/listener.ts
+++ nats_test_project/src/listener.ts
@@ -10,6 +10,18 @@ const stan = nats.connect("microticket", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to nats");
 
+  // DODACU JOS JEDNOG LISTNERA OVDE
+  // OVO CE SE IZVRSITI SVAKI PUT KADA SE POKUSA CLOSING CLIENT-A
+  // ODNOSNO NJEGOV DISCONECTING FROM THE RUNNING SERVER
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+
+    // RADIMO process.exit() STO CE NAS KICK-OVATI OUT
+    // RECI END THIS PROCESS I DON'T DO ANYTHING ELLSE
+    process.exit();
+  });
+  //
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
 
   const subscription = stan.subscribe(
@@ -27,7 +39,24 @@ stan.on("connect", () => {
 
     console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
 
-    // EVO OVDE CEMO TO URADITI
     msg.ack();
   });
 });
+
+// A OVDE NA KRAJU DODAJEM DVA HANDLERA, KOJU WATCH-UJU ALL THE TIME
+// DA LI NEKO ZATVARA OVAJ PROCESS
+// DAKLE OVO JE HANDLER KOJI SLUSA KADA NEKO ODRADI
+// Ctrl + C
+// USTVARI PRVI JE HANDLER ZA INTERUPT SIGNAL, A DRUGI JE
+// SIGNAL ZA TERMINATE SIGNAL
+process.on("SIGINT", () => {
+  // KADA SE TO DESI RADIMO
+  stan.close();
+});
+process.on("SIGTERM", () => {
+  stan.close();
+});
+
+// DAKLE OVO ZNACI DA KADA PRITISNES CTRL + C ILI KADA
+// POKUSAS DA CLOSE-UJES KONEKCIJU NNATS STREAMING SERVERA
+// RECI CE SE HEJ SACEKAJ DA UNISTIM OVOG CLIENT-A
