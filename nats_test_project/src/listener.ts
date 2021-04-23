d7bce3ac755896f3edc8131f8b16db46f94c4cd4
Rade58 <bajic.rade2@gmail.com>
2021-04-23 10:50:00 +0200

    🦉

--- nats_test_project/src/listener.ts
+++ nats_test_project/src/listener.ts
@@ -1,11 +1,8 @@
 import nats, { Message } from "node-nats-streaming";
-
-// EVO SA OVIM CU DA GENERISEM RANDOM STRING
 import { randomBytes } from "crypto";
 
 console.clear();
 
-// EVO VIDIS, GENERISEM ID
 const stan = nats.connect("microticket", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
@@ -16,17 +13,20 @@ stan.on("connect", () => {
   const subscription = stan.subscribe("ticket:created");
 
   subscription.on("message", (msg: Message) => {
-    const eventNumber = msg.getSequence();
-    const topic = msg.getSubject();
-    console.log({ topic, eventNumber });
+    // const eventNumber = msg.getSequence();
+    // const topic = msg.getSubject();
+    // console.log({ topic, eventNumber });
     const data = msg.getData();
 
     if (typeof data === "string") {
       const dataObject = JSON.parse(data);
 
-      console.log(dataObject.title);
-      console.log(dataObject.id);
-      console.log(dataObject.price);
+      // console.log(dataObject.title);
+      // console.log(dataObject.id);
+      // console.log(dataObject.price);
     }
+    // EVO ZELIM DA STMAPAM OVAKAV STRING
+    console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    //
   });
 });
