0e966aa40dd38dbc34b8fb421f129b367980c4ed
Rade58 <bajic.rade2@gmail.com>
2021-05-01 14:16:03 +0200

    🦉

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,29 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { requireAuth, validateRequest } from "@ramicktick/common";
+import { body } from "express-validator";
+import { Types as MongooseTypes } from "mongoose";
 
 const router = Router();
 
-router.post("/api/orders", async (req, res) => {
-  //
-  res.send({});
-});
+router.get(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .isString()
+      .not()
+      .isEmpty()
+      .custom((input: string) => {
+        return MongooseTypes.ObjectId.isValid(input);
+      })
+      .withMessage("'ticketId' is invalid or not provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    res.send({});
+  }
+);
 
 export { router as createNewOrderRouter };
