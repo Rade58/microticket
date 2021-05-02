1c9f1dc60193657f6d7625636ca7fdeb15b50e82
Rade58 <bajic.rade2@gmail.com>
2021-05-02 17:34:56 +0200

    🦉

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,48 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import {
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError,
+  requireAuth,
+  OrderStatusEnum as OSE,
+} from "@ramicktick/common";
+import { isValidObjectId } from "mongoose";
+import { Order } from "../models/order.model";
 
 const router = Router();
 
-router.delete("/api/orders/:orderId", async (req, res) => {
-  //
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    if (isValidObjectId(orderId)) {
+      throw new BadRequestError("order id is invalid mongodb object id");
+    }
+
+    // PRONALAZENJE DOKUMENT
+    let order = await Order.findOne({ _id: orderId }).exec();
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req?.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // MENJANJE STATUSA ORDERU
+
+    order = await Order.findOneAndUpdate(
+      { _id: orderId },
+      { status: OSE.cancelld }
+    )
+      .populate("ticket")
+      .exec();
+
+    res.status(201).send(order);
+  }
+);
 
 export { router as deleteSingleOrderRouter };
