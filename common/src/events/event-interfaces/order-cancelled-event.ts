68c25a4da9cd19f06179fef4369139d9bba90e22
Rade58 <bajic.rade2@gmail.com>
2021-05-05 15:23:42 +0200

    🦉

--- common/src/events/event-interfaces/order-cancelled-event.ts
+++ common/src/events/event-interfaces/order-cancelled-event.ts
@@ -4,6 +4,9 @@ export interface OrderCancelledEventI {
   channelName: CNE.order_cancelled;
   data: {
     id: string;
+    // EVO U SKLOPU data, ZADAO SAM TAJ     version
+    version: number;
+    //
     ticket: {
       id: string;
     };
