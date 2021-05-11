23260bc460daa7ff1c38f8dbbc5a0af88732fe35
Rade58 <bajic.rade2@gmail.com>
2021-05-11 21:58:21 +0200

    🦉

--- payments/src/models/order.model.ts
+++ payments/src/models/order.model.ts
@@ -59,17 +59,29 @@ export interface OrderDocumentI extends Document, OrderFields {
  * @description interface for additional things on the model (MOSTLY METHODS TO BE USED ON THE MODEL)
  */
 interface OrderModelI extends Model<OrderDocumentI> {
-  // NECU NISTA DODAVATI, ALI OVDE BI TYPE-OVAO STATICKE METODE KOJE
-  // SAMO TI OSTAVLJAM OVO KAO TEMPLATE DEFINISANJA
-  __nothing: (input: string) => void; //stavio samo jer moram nesto da dodam, ali ovu metodu necu sigurno definisati
+  findOneByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<OrderDocumentI | null>;
 }
 
 // BUILDING STATIC METHODS ON MODEL ( JUST SHOVING NOT GOING TO USE IT )
 // orderSchema.statics.__nothing = async function (input) {/**/};
 // pre HOOK
 // orderSchema.pre("save", async function (next) {/**/});
-// METHODS ON MODEL
+// METHODS ON DOCUMENT
 // orderSchema.methods.
+
+orderSchema.statics.findByEvent = async function (event: {
+  id: string;
+  version: number;
+}) {
+  const { id, version } = event;
+
+  const order = await this.findOne({ _id: id, version: version - 1 });
+
+  return order;
+};
 
 const Order = model<OrderDocumentI, OrderModelI>("Order", orderSchema);
 
