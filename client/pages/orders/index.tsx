b164b02f20ba418cc1d59bd107583b1b8e51a8a0
Rade58 <bajic.rade2@gmail.com>
2021-05-17 15:41:30 +0200

    🦉

--- client/pages/orders/index.tsx
+++ client/pages/orders/index.tsx
@@ -2,34 +2,51 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import { FunctionComponent } from "react";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { OrderStatusEnum as OSE } from "@ramicktick/common";
 import { InitialPropsI } from "../../types/initial-props";
 import { buildApiClient } from "../../utils/buildApiClient";
+import { OrderDataTicketPopulatedI } from "../../types/data/order-data";
 
 interface PropsI extends InitialPropsI {
-  placeholder: boolean;
+  orders: OrderDataTicketPopulatedI[];
 }
 
 export const getServerSideProps: GetServerSideProps<PropsI> = async (ctx) => {
   const client = buildApiClient(ctx);
 
-  const orders = await client.get("/api/orders");
+  // IMAS ORDER SA POPULATED ticket FIELD-OM
+  const { data } = await client.get("/api/orders");
 
-  console.log({ orders: orders.data });
-
+  // RETURN-UJEM orders KAO PROP
   return {
     props: {
-      placeholder: true,
+      orders: data,
     },
   };
 };
 
 const IndexPage: FunctionComponent<PropsI> = (props) => {
-  //
+  const { orders } = props;
 
-  console.log(props);
-
-  // eslint-disable-next-line
-  return <div>🦉</div>;
+  return (
+    <ul>
+      {orders.map(({ ticket: { title }, status, id }) => {
+        return (
+          <li key={id}>
+            {status === OSE.complete || status === OSE.cancelled ? (
+              <>{title}</>
+            ) : (
+              <Link href={`/orders/${id}`}>
+                <a>{title}</a>
+              </Link>
+            )}
+            - {status}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default IndexPage;
