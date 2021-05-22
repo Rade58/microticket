ca1a765e3ed521d2d4dbf53ef63b39f924dfc6cc
Rade58 <bajic.rade2@gmail.com>
2021-05-22 18:01:37 +0200

    🦉

--- client/utils/buildApiClient.ts
+++ client/utils/buildApiClient.ts
@@ -6,9 +6,14 @@ export const buildApiClient = (
   ctx?: GetServerSidePropsContext | NextPageContext
 ) => {
   const isServerSide = isSSR();
+  // OVO JE PROBLEM
+  // ZBOG GRESKE NA DIGITAL OCEAN-U KOJA IMA VEZE SA
+  // INGRESS-OM, MORAO SAM DODATI DODATNU KONFIGURACIJU ZA INGRESS
+  // ISTO TAKO NISAM MOGAO DA KORISTIM, OVAJ URL
   /* const baseURL =
     "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local" */;
-  const baseURL =
+  // VEC OVAJ
+    const baseURL =
     "http://www.microticket.xyz/";
 
   if (isServerSide && ctx) {
