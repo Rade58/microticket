dd326bb459f9905b87f7c030f968e38a2c1f417b
Rade58 <bajic.rade2@gmail.com>
2021-05-24 23:42:13 +0200

    🦉

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -19,10 +19,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    // EVO OVO CEMO UMESTO OVOGA
-    // secure: process.env.NODE_ENV !== "test",
-    // PROSTO PODESITI DA BUDE `false`
-    secure: false
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
