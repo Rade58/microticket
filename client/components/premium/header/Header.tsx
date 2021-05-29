ab71a1d03d382498dc3aab601ad5b269a52b4d40
Rade58 <bajic.rade2@gmail.com>
2021-05-29 17:28:52 +0200

    🦉

---
+++ client/components/premium/header/Header.tsx
@@ -0,0 +1,11 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { FunctionComponent } from "react";
+
+const Header: FunctionComponent = () => {
+  return null;
+};
+
+export default Header;
